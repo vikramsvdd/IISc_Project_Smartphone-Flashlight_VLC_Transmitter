d4a1848a5c85595745c600d41a96cd3ac06029c5
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2022-05-25 23:26:41 +0530

    Add files via upload

---
+++ ArduinoFiles/pwm_sketch_may25a.ino
@@ -0,0 +1,57 @@
+#define BUTTON_PIN 3
+word pulseWidth[100];
+volatile unsigned long pulseInTimeBegin = micros();
+volatile unsigned long pulseInTimeEnd = micros();
+volatile bool newPulseDurationAvailable = false;
+int c=0;
+
+void buttonPinInterrupt()
+{
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    // start measuring
+    pulseInTimeBegin = micros();
+  }
+  else {
+    // stop measuring
+    pulseInTimeEnd = micros();
+    newPulseDurationAvailable = true;
+  }
+}
+
+void setup() {
+  for (int i = 0; i < 100; i++) {
+    pulseWidth[i] = 0; // clears the array
+  }
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN),
+                  buttonPinInterrupt,
+                  CHANGE);
+}
+
+void loop() {
+  if (newPulseDurationAvailable) {
+    newPulseDurationAvailable = false;
+    unsigned long pulseDuration = pulseInTimeEnd - pulseInTimeBegin;
+    //Serial.println(pulseDuration);
+    pulseWidth[c] =pulseDuration ;
+    c+=1;
+    //Serial.println(c);
+  }
+
+    if (c==100) { // print 100 results
+    for (int i = 0; i < 100; i++) {
+     // Serial.print(i + 1);
+      //Serial.print(": ");
+      Serial.println(pulseWidth[i]); // the pulse width in μs
+      //Serial.println(" μs");
+    }
+    for (int i = 0; i < 100; i++) {
+      pulseWidth[i] = 0; // clears the array
+    }
+
+  }
+
+  // do your other stuff here
+}
