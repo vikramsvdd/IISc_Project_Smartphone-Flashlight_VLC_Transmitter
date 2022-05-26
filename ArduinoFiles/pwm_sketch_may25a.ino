3364a4afdd728ee6f0a35f9f3c66ef0fd3731c07
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2022-05-27 00:50:23 +0530

    Update pwm_sketch_may25a.ino

--- ArduinoFiles/pwm_sketch_may25a.ino
+++ ArduinoFiles/pwm_sketch_may25a.ino
@@ -1,10 +1,11 @@
 #define BUTTON_PIN 3
-word pulseWidth[96];
+word pulseWidth[97];
 volatile unsigned long pulseInTimeBegin = micros();
 volatile unsigned long pulseInTimeEnd = micros();
-volatile bool newPulseDurationAvailable = false;
+ volatile bool newPulseDurationAvailable = false;
+ int a;
 int c=0;
-
+ 
 void buttonPinInterrupt()
 {
   if (digitalRead(BUTTON_PIN) == HIGH) {
@@ -19,48 +20,49 @@ void buttonPinInterrupt()
 }
 
 void setup() {
-  for (int i = 0; i < 96; i++) {
+  for (int i = 0; i < 97; i++) {
     pulseWidth[i] = 0; // clears the array
   }
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(BUTTON_PIN, INPUT);
 
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN),
                   buttonPinInterrupt,
-                  HIGH);
-                  }
+                  CHANGE);
+}
 
 void loop() {
   if (newPulseDurationAvailable) {
     newPulseDurationAvailable = false;
     unsigned long pulseDuration = pulseInTimeEnd - pulseInTimeBegin;
-    //Serial.println(pulseDuration);
-    pulseWidth[c] =pulseDuration ;
+   // Serial.println(pulseDuration);
     c+=1;
-    //Serial.println(c);
+    pulseWidth[c-1] =pulseDuration ;
+    a=pulseDuration;
+   
+    //Serial.println(pulseDuration);
   }
 
-    if (c==95) { // print 100 results
+    if (c==96) { // print 100 results
     for (int i = 0; i < 96; i++) {
      // Serial.print(i + 1);
       //Serial.print(": ");
-     // Serial.println(pulseWidth[i]); // the pulse width in μs
-      if(pulseWidth[i]>5000){
+      //pulseWidth[99]=a;
+      //Serial.println(pulseWidth[i]); // the pulse width in μs
+      if(pulseWidth[i]>=10000){
         Serial.println(1);
         }
       else{
         Serial.println(0);
-      } 
-      
+      }
       //Serial.println(" μs");
     }
-    
-    for (int i = 0; i < 96; i++) {
+    for (int i = 0; i < 97; i++) {
       pulseWidth[i] = 0; // clears the array
       c=0;
     }
 
-    }
+  }
 
   // do your other stuff here
-  }
+}
