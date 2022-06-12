147127043a6205c42ef322b5cc404ba588ad3eda
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2022-06-12 11:59:16 +0530

    Update cpp-prog-for-controlling-flashlight-through-shell.cpp

--- Cpp_files_shell/cpp-prog-for-controlling-flashlight-through-shell.cpp
+++ Cpp_files_shell/cpp-prog-for-controlling-flashlight-through-shell.cpp
@@ -4,6 +4,7 @@ int main()
 {
 //  system("su");
 system("cd ~ | echo 100 > ~/sys/class/leds/flashlight/brightness");
+ //system("sh script_for_testing_flashlight.sh");
  // system("echo 100 > ~/sys/class/leds/flashlight/brightness ");
   
   return 0;
